8b576ea7444d7f9b19a759fef34aa958777f9b82
Carl Kadie <carlk@msn.com>
2022-01-25 14:48:18 -0800

    Fix windows build problems

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,6 @@
 REM Based on bld.bat from pysyntect-feedstock
 REM https://github.com/conda-forge/pysyntect-feedstock/
 
-REM Create temp folder
-mkdir tmpbuild_%PY_VER%
-set TEMP=%CD%\tmpbuild_%PY_VER%
 REM Print Rust version
 rustc --version
 
@@ -15,4 +12,4 @@ REM set UTF-8 mode by default
 chcp 65001
 set PYTHONUTF8=1
 set PYTHONIOENCODING="UTF-8"
-FOR %%w in (*.whl) DO %PYTHON% -m pip install %%w --build tmpbuild_%PY_VER%
+FOR %%w in (*.whl) DO %PYTHON% -m pip install %%w
